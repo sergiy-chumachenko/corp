2c33413bfc3591c2e0dcd15b00af72b4f6f2ae09
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2019-10-12 17:40:22 +0300

    'JobTitle, Employee, City' models has been added & admin side has been configured

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from company.models import JobTitle, Employee, City
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    fields = ('name',)
+
+
+@admin.register(JobTitle)
+class JobTitleAdmin(admin.ModelAdmin):
+    fields = ('name',)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'job_title', 'city')
+    list_filter = ('city',)
--- company/models.py
+++ company/models.py
@@ -1,3 +1,48 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+
+
+class JobTitle(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Job Title'
+        verbose_name_plural = 'Job Titles'
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    city = models.ForeignKey(
+        City,
+        related_name='employee_city',
+        on_delete='cascade'
+    )
+    job_title = models.ForeignKey(
+        JobTitle,
+        related_name='employee_job_title',
+        on_delete='cascade'
+    )
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employes'
+        index_together = [
+            ['first_name', 'last_name']
+        ]
